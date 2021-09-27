7929f8391de2055537fb5aac83b6b292ab489f54
dylson <dylsonuni@gmail.com>
2021-09-27 18:22:05 +0100

    populated the seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,24 @@
+DROP DATABASE IF EXISTS  main_db;
 CREATE DATABASE main_db;
 
 USE main_db;
 
 CREATE TABLE department (
-id INT PRIMARY KEY;
-name VARCHAR(30); /* this will hold the department name */
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(30) /* this will hold the department name */
 )
 
 CREATE TABLE role (
-    id INT PRIMARY KEY;
-    title VARCHAR(30); /* this will hold the role title */
-    salary DECIMAL /* this will hold the role salary */
-    department_id INT /* to hold reference to department role belongs to */
+    id INT PRIMARY KEY NOT NULL,
+    title VARCHAR(30), /* this will hold the role title */
+    salary DECIMAL, /* this will hold the role salary */
+    department_id INT NOT NULL /* to hold reference to department role belongs to */
 )
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY;
-    first_name VARCHAR(30); /* to hold employee first name */
-    last_name VARCHAR(30); /* to hold employee last name */
-    role_id INT /* to hold reference to employee role */
-    manager_id INT /* to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)*/
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL, /* to hold employee first name */
+    last_name VARCHAR(30) NOT NULL, /* to hold employee last name */
+    role_id INT NOT NULL, /* to hold reference to employee role */
+    manager_id INT NOT NULL /* to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)*/
 )
