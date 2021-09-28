bce7a856c0d50480e1b8be4c077c4df810d091b7
dylson <dylsonuni@gmail.com>
2021-09-28 16:39:27 +0100

    connected the database to the js file

--- schema.js
+++ schema.js
@@ -1,2 +1,21 @@
 /* Bringing in the mysql2 package */
 const mysql = require("mysql2");
+
+/* Connecting to the database */
+const db = mysql.createConnection(
+    {
+        host:"localhost",
+        user:"root",
+        password:"",
+        database:"main_db"
+    },
+    console.log("connected to the main_db database")
+);
+
+/* Query databse */
+
+const viewDepartment = db.query("SELECT * FROM department", function (err, results) {
+    console.log(results);
+})
+
+module.exports = viewDepartment;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 /* Bringing all the packages and exports necessarry for the app. */
 const inquirer = require("inquirer");
 const menuPrompt = require("./Prompts/menuPrompt");
+const viewDepartment = require("./schema");
 
 /* This array will later contain all the employees created */
 const employeeArr = [];
 
+/* I need to make this async now! */
 const Start = () => {
   inquirer.prompt(menuPrompt).then((answers) => {
         /* inquirer is returning an object with key/values based on the names of the questions. So to read the data from your menu prompt you need the following console.log...*/
@@ -14,6 +16,7 @@ const Start = () => {
         /* Now I want to show the "department" table I created in the db folder.*/
         /* console.table(department); <-- This does not work, I need to know how to fetch this data from another file. Is it just module.exports?  */
         console.log("Entered the if statement for View All Departments");
+        viewDepartment;
     }
   });
   console.log("Start function has ended");
