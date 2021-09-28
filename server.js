7be8bd3268be0792cd49bd0bed3a080362382a9f
dylson <dylsonuni@gmail.com>
2021-09-28 16:52:05 +0100

    bringing in util package

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 /* Bringing all the packages and exports necessarry for the app. */
 const inquirer = require("inquirer");
+const util = require("util");
 const menuPrompt = require("./Prompts/menuPrompt");
 const viewDepartment = require("./schema");
 
 /* This array will later contain all the employees created */
 const employeeArr = [];
 
+/* Playing around with ways to make start function asynchronous*/
+const makeFuncAsync = util.promisify(Start)
+
 /* I need to make this async now! */
 const Start = () => {
-  inquirer.prompt(menuPrompt).then((answers) => {
+  inquirer
+  .prompt(menuPrompt)
+  .then((answers) => {
         /* inquirer is returning an object with key/values based on the names of the questions. So to read the data from your menu prompt you need the following console.log...*/
         console.log( `You have selected: ${answers.mainMenu}`); 
 
