eab04e5f6c4dc71f18c60b21e6afca41f0f5b607
Dylson <dylsonuni@cgmail.com>
2021-10-02 16:34:23 +0100

    finished the query for viewEmployees and brought in npm package

--- db/queries.js
+++ db/queries.js
@@ -13,9 +13,11 @@ const viewRoles = () => {
     })
 };
 
-const viewEmployees = () => [
-    db.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name,' ',manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN manager ON manager.id = employee.manager_id")
-]
+const viewEmployees = () => {
+    db.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name,' ',manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN manager ON manager.id = employee.manager_id", function (err, results) {
+        console.table(results);    
+    })
+};
 
 
 module.exports = {viewDepartments, viewRoles, viewEmployees};
