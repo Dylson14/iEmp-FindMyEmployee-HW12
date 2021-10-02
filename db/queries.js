a34d307d58eef241cf181b6d7fdca29e10ca4f31
Dylson <dylsonuni@cgmail.com>
2021-10-02 17:00:44 +0100

    final touches

--- db/queries.js
+++ db/queries.js
@@ -14,10 +14,13 @@ const viewRoles = () => {
 };
 
 const viewEmployees = () => {
-    db.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name,' ',manager.last_name) AS manager FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN manager ON manager.id = employee.manager_id", function (err, results) {
+    db.query("SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN manager ON manager.id = employee.manager_id", function (err, results) {
         console.table(results);    
     })
 };
 
 
 module.exports = {viewDepartments, viewRoles, viewEmployees};
+
+
+/* CONCAT(manager.first_name,' ',manager.last_name) AS manager */
